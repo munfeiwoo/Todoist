2dfa865b5bcba6cfd3f5c9177461fa2b5f7041c0
Mun Fei Woo <munfei.woo@gmail.com>
2020-05-25 16:58:22 +0800

    added logging

--- mobile/test_todoist_task.py
+++ mobile/test_todoist_task.py
@@ -1,5 +1,7 @@
 import pytest
 import time
+import logging
+
 
 from API.test_todoist_project import api_create_new_project, api_delete_project, api_get_project_details
 from API.test_todoist_task import api_get_project_task_by_name, api_get_project_task_id_by_name, api_reopen_task
@@ -8,6 +10,9 @@ from pages.todoist_leftNav import TodoistLeftNav
 from pages.todoist_project import TodoistProject
 from mobile.generic import TodoistGeneric
 
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger()
+
 
 @pytest.mark.P1
 @pytest.mark.Task
@@ -15,6 +20,7 @@ from mobile.generic import TodoistGeneric
 def test_task_creation(app, api, user):
 
     # Setup
+    log.info("Setting up test environment to create_project")
     project_name = 'Setel Project2'
     task_title = 'testing 123'
     task_datetime = '26 June 2020 11am'
@@ -23,33 +29,45 @@ def test_task_creation(app, api, user):
     project_task_url = api['project_task_url']
     email = user['email']
     password = user['password']
+    log.info("Create project using API")
     project_id = api_create_new_project(token, project_url,project_name)
     assert (project_id > 0)
+    log.info("Project created successfully")
 
     # Body
+    log.info("Perform login")
     login = TodoistLogin(app)
     login.email_login(email, password)
     time.sleep(3)
+    log.info("Login successfully")
 
+    log.info("Goto Left Nav and select a project")
     left_nav = TodoistLeftNav(app)
     left_nav.get_main_menu()
     left_nav.select_project_option()
     left_nav.select_a_project_by_project_name(project_name)
+    log.info("Project selected")
 
+    log.info("Add task for a project")
     project_page = TodoistProject(app)
     project_page.select_add_task()
     project_page.add_task_title(task_title)
     project_page.submit_task_schedule(task_datetime)
     project_page.submit_schedule()
     project_page.submit_task()
+    log.info("Task created for the project")
     time.sleep(5)
+
+    log.info("Verify task created using API")
     assert (api_get_project_task_by_name(token, project_task_url, project_id, task_title) is not None)
+    log.info("Task exist from API verification")
 
     # Teardown
+    log.info("Remove and clean up project")
     api_delete_project(token, project_url, project_id)
     response = api_get_project_details(token, project_url,project_id)
     assert (response.status_code == 404)
-
+    log.info("Project removed successfully")
 
 @pytest.mark.P1
 @pytest.mark.Task
@@ -57,6 +75,7 @@ def test_task_creation(app, api, user):
 def test_reopen_task(app, api, user):
 
     # Setup
+    log.info("Setting up test environment to create_project")
     project_name = 'Setel Project3'
     task_title = 'testing 123'
     task_datetime = '26 June 2020 11am'
@@ -65,19 +84,27 @@ def test_reopen_task(app, api, user):
     project_task_url = api['project_task_url']
     email = user['email']
     password = user['password']
+
+    log.info("Create project using API")
     project_id = api_create_new_project(token, project_url, project_name)
     assert (project_id > 0)
+    log.info("Project created successfully")
 
     # Body
+    log.info("Perform login")
     login = TodoistLogin(app)
     login.email_login(email, password)
     time.sleep(3)
+    log.info("Login successfully")
 
+    log.info("Goto Left Nav and select a project")
     left_nav = TodoistLeftNav(app)
     left_nav.get_main_menu()
     left_nav.select_project_option()
     left_nav.select_a_project_by_project_name(project_name)
+    log.info("Project selected")
 
+    log.info("Add task to a project")
     project_page = TodoistProject(app)
     project_page.select_add_task()
     project_page.add_task_title(task_title)
@@ -85,27 +112,45 @@ def test_reopen_task(app, api, user):
     project_page.submit_schedule()
     project_page.submit_task()
     time.sleep(3)
+    log.info("Task created for the project")
 
+    log.info("API CALL: Get task id by task title")
     task_id = api_get_project_task_id_by_name(token, project_task_url, project_id, task_title)
+    assert(task_id > 0)
+    log.info("API CALL: Task id returned successfully")
     app.back()
 
+    log.info("Navigate again to the project")
     time.sleep(5)
     left_nav.get_main_menu()
     time.sleep(3)
     left_nav.select_a_project_by_project_name(project_name)
     time.sleep(5)
+    log.info("Navigated successfully")
+
+    log.info("Mark task to complete")
     project_page.mark_completed_project_task_by_name(task_title)
     time.sleep(5)
+    log.info("Task marked to complete successfully")
 
+    log.info("API CALL: Reopen Task")
     api_reopen_task(token, project_task_url, task_id)
+    log.info("API CALL completed")
+
+    log.info("Perform data sync in Mobile")
     TodoistGeneric.sync_data(app)
 
+    log.info("Navigate back to project")
     time.sleep(5)
     left_nav.select_a_project_by_project_name(project_name)
     time.sleep(5)
     assert (project_page.get_project_task_by_name(task_title) is not None)
+    log.info("Task has been reopened successfully and displaying")
 
     # Teardown
+
+    log.info("Remove and clean up project")
     api_delete_project(token, project_url, project_id)
     response = api_get_project_details(token, project_url, project_id)
     assert (response.status_code == 404)
+    log.info("Project removed successfully")
